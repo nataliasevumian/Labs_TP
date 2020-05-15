386c4f14ea9bfc300ebe0a19f60b05e1a3d0327a
nataliasevumian <62780776+nataliasevumian@users.noreply.github.com>
2020-05-16 00:48:48 +0300

    Add files via upload

---
+++ IProgression.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab_6
+{
+    interface IProgression
+    {
+        double GetElement(int k);
+    }
+}
---
+++ IPubs.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab_6
+{
+    interface IPubs
+    {
+        void Subs(); 
+        bool IfSubs { get; set; }
+    }
+}
